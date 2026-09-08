fa2449eb0a1376ae587f538f3b0bc9975114cfa3
jhonatanT04 <venotacu@gmail.com>
2026-09-07 21:13:30 -0500

    fix: update CORS configuration to allow specific origins

--- backend/src/main/java/ec/cue/backend/config/SecurityConfig.java
+++ backend/src/main/java/ec/cue/backend/config/SecurityConfig.java
@@ -68,7 +68,9 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:5173"));
+		config.setAllowedOrigins(List.of(
+            "https://taller-costura-gray.vercel.app"
+        ));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 		config.setAllowCredentials(true);
