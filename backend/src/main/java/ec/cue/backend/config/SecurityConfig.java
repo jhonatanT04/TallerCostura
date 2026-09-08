0f50e5888dd638d7cd8b0ae8d619567fcb77676a
jhonatanT04 <venotacu@gmail.com>
2026-09-07 21:44:02 -0500

    fix: update CORS configuration to allow localhost for development

--- backend/src/main/java/ec/cue/backend/config/SecurityConfig.java
+++ backend/src/main/java/ec/cue/backend/config/SecurityConfig.java
@@ -69,7 +69,8 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOrigins(List.of(
-            "https://taller-costura-gray.vercel.app"
+            "https://taller-costura-gray.vercel.app",
+			"http://localhost:5173"
         ));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
